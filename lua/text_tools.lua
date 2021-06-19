064900c54d6d82d2aef454a4d6fa2283aa0d4189
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-06-19 13:09:47 +0200

    moved from nvim config to seperate plugin

--- lua/text_tools.lua
+++
@@ -1,9 +0,0 @@
--- which key
-require("which-key").setup {}
-
-require("kommentary.config").configure_language("default", {
-	prefer_single_line_comments = true,
-})
-
--- should probaly use autocommand to chance settings for latex
--- require("spellsitter").setup()
