2d0ce4042ad5538053fb1730c1137e4ec019f35e
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-07-19 23:49:15 +0200

    adds util function to print hl group under cursor

--- lua/prosesitter.lua
+++ lua/prosesitter.lua
@@ -9,7 +9,9 @@ M.hover = require("prosesitter/hover") -- exposed for keybindings
 
 local attached = {}
 local function on_win(_, _, bufnr)
-	if disabled then return true end  -- stop calling on lines if the plugin was just disabled
+	if disabled then
+		return true
+	end -- stop calling on lines if the plugin was just disabled
 
 	if not attached[bufnr] then
 		attached[bufnr] = true
@@ -54,7 +56,7 @@ local es = nil
 function M.test1()
 	local start = 3
 	local stop = 3
-	es = api.nvim_buf_get_extmarks(0, shared.ns_marks, {start,0}, {stop,-1}, {})
+	es = api.nvim_buf_get_extmarks(0, shared.ns_marks, { start, 0 }, { stop, -1 }, {})
 	log.info(vim.inspect(es))
 end
 
@@ -62,6 +64,35 @@ function M.test2()
 	for _, e in ipairs(es) do
 		local m = api.nvim_buf_get_extmark_by_id(0, shared.ns_marks, e[1], {})
 		log.info(vim.inspect(m))
+	end
+end
+
+function M.showhl()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	row = row -1
+	col = col -1
+	local parser = vim.treesitter.get_parser(0)
+	local lang = parser:lang()
+
+	local query = require("vim.treesitter.query")
+	local hl = query.get_query(lang, "highlights")
+	local hl_group = nil
+
+	parser:for_each_tree(function(tstree, _)
+		local root = tstree:root()
+		for id, node in hl:iter_captures(root, 0) do
+			local start_row, start_col, end_row, end_col = node:range()
+			if start_row > row or end_row < row then goto continue end
+			if start_col > col or end_col < col then goto continue end
+			hl_group = hl.captures[id]
+			::continue::
+		end
+	end)
+
+	if hl_group ~= nil then
+		print("cursor now on treesitter highlight: '"..hl_group.."'")
+	else
+		print("no highlight group under cursor")
 	end
 end
 
