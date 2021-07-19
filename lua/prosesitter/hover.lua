d07b3b93a5783051b052e7bcf40f04990162cc1b
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-07-19 15:17:12 +0200

    replaced hover with vim.lsp.util.open_floating_preview

--- lua/prosesitter/hover.lua
+++ lua/prosesitter/hover.lua
@@ -12,54 +12,7 @@ function M.setup(_shared)
 	shared = _shared
 end
 
-local org_cursor = nil
-local function hide_cursor()
-	org_cursor = vim.opt.guicursor
-	vim.cmd("hi Cursor blend=100")
-	vim.opt.guicursor = { "a:Cursor/lCursor" }
-end
-
-local function restore_cursor()
-	vim.opt.guicursor = org_cursor
-end
-
-local function map_keys(buf)
-	local opt = { nowait = true, noremap = true, silent = true }
-	local cmd = ":lua _G.ProseSitter.hover:close_popup()<CR>"
-	local chars = "1234567890abcdefghijklmnopqrstuvwxyz/;.,"
-	for i = 1, #chars do
-		local key = chars:sub(i, i)
-		api.nvim_buf_set_keymap(buf, "n", key, cmd, opt)
-		api.nvim_buf_set_keymap(buf, "n", key:upper(), cmd, opt)
-	end
-
-	local special_keys = {
-		"<Right>",
-		"<Left>",
-		"<Up>",
-		"<Down>",
-		"<Space>",
-		"<LeftMouse>",
-		"<RightMouse>",
-		"<Esc>",
-		"<CR>",
-		"<Tab>",
-		"<Del>",
-	}
-	for _, key in ipairs(special_keys) do
-		api.nvim_buf_set_keymap(buf, "n", key, cmd, opt)
-	end
-end
-
-local win = nil
-function M.close_popup()
-	api.nvim_win_close(win, true)
-	win = false
-	restore_cursor()
-end
-
--- open hover window if lint error on current pos
--- else return false
+-- open hover window if lint error on current pos else return
 function M.popup()
 	local row, col = unpack(api.nvim_win_get_cursor(0))
 	local start = { row - 1, col } -- row needs to be zero indexed for get_extmarks
@@ -72,25 +25,7 @@ function M.popup()
 	local id = es[1][1]
 	local text = shared.mark_to_hover[id]
 
-	local buf = api.nvim_create_buf(false, true)
-	api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-	api.nvim_buf_set_option(buf, "filetype", "whid")
-	api.nvim_buf_set_lines(buf, 0, -1, false, { text })
-	api.nvim_buf_set_option(buf, "modifiable", false)
-	hide_cursor()
-	-- https://dev.to/2nit/how-to-write-neovim-plugins-in-lua-5cca
-
-	local opt = {
-		style = "minimal",
-		relative = "cursor",
-		width = math.min(40, #text),
-		height = 2,
-		row = 1,
-		col = 0,
-	}
-
-	win = api.nvim_open_win(buf, true, opt)
-	map_keys() -- make any key close the window
+	vim.lsp.util.open_floating_preview({text}, "markdown", {})
 end
 
 return M
