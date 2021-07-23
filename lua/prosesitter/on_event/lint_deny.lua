3b8a2515b8c3f0adbb9f9a9fe1cc4734a9bd13aa
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-07-23 19:14:50 +0200

    draft of deny lint, still missing features

--- lua/prosesitter/on_event/lint_deny.lua
+++ lua/prosesitter/on_event/lint_deny.lua
@@ -6,50 +6,13 @@ local M = {}
 M.__index = M -- failed table lookups on the instances should fallback to the class table, to get methods
 function M.new()
 	local self = setmetatable({}, M)
-	self.text = {}
 	self.meta_by_mark = {}
-	self.meta_by_idx = {}
 	return self
 end
 
-function M:update(marks, buf, row, start_col, end_col)
-	-- remove all but first mark on the current line, change that first mark
-	-- into a placeholder mark
-	local id = marks[1][1] -- there can be a max of 1 placeholder per line
-	local opt = { id = id, end_col= end_col }
-	api.nvim_buf_set_extmark(buf, M.ns, row, start_col, opt) -- update placeholder
-	local full_new_line = api.nvim_buf_get_lines(buf, row, row+1, true)[1]
-	local new_line = string.sub(full_new_line, start_col, end_col)
-	local idx = self.meta_by_mark[id].text_idx
-	self.text[idx] = new_line
-end
-
--- only single lines are added... issue if line breaks connect scentences
-function M:add(buf, row, start_col, end_col)
-	-- TODO do we want start_col till end coll? 
-	-- what happens if a comment is moved further from the line start?
-	local marks = api.nvim_buf_get_extmarks(buf, ns, {row, start_col}, {row, end_col}, {})
-	if #marks > 0 then
-		self:update(marks, buf, row, start_col, end_col)
-		return
-	end
-
-	local opt = { end_col= end_col }
-	local ok, placeholder_id = pcall(api.nvim_buf_set_extmark, buf, ns, row, start_col, opt)
-	if ok == false then
-		log.info("could not add placeholder: ",buf, ns, row, start_col, end_col)
-	end
-	local full_line = api.nvim_buf_get_lines(buf, row, row+1, true)
-	local line = string.sub(full_line[1], start_col, end_col)
-	self.text[#self.text+1] = line
-
-	local meta = {buf=buf, text_idx=#self.text, id=placeholder_id}
-	self.meta_by_mark[placeholder_id] = meta
-	self.meta_by_idx[#self.text] = meta
-end
 
 function M:is_empty()
-	local empty = next(self.text) == nil
+	log.error("not implemented")
 	return empty
 end
 
@@ -59,7 +22,7 @@ local function to_string(table)
 	-- local array = {}
 	local text = ""
 	for _, v in pairs(table) do
-		text=text..v.." "
+		text = text .. v .. " "
 		-- array[#array+1] = v
 	end
 	-- local text = table.concat(array, "\n", 1, 2)
@@ -67,22 +30,63 @@ local function to_string(table)
 end
 
 function M:build()
+	local text = {}
 	local req = {}
-	req.text = to_string(self.text)
 	req.meta_array = {}
 
-	local col = 0
-	for i=1,#self.text do
-		local meta = self.meta_by_idx[i]
-		meta.col = col
-		req.meta_array[#req.meta_array+1] = meta
-		col = col + #self.text[i] + 1 -- plus one for the line end
+	local marks = sorted_marks()
+	for _, line_id in ipairs(marks) do
+		local line_txt = get_line(line_id)
+		local line_meta = self.meta_by_mark[line_id]
+		if line_meta == nil then
+			text[#text+1] = line_txt
+		end
+
+		for _, meta in ipairs() do
+			text[#text+1] = line_txt:sub(meta.start_col, meta.end_col)
+		end
 	end
 
 	return req
 end
 
-function M:cfg_on_lines(buf, nodes)
+function M:ensure_placeholders(buf, start_l, end_l)
+	local marks = api.nvim_buf_get_extmarks(buf, ns, { start_l, 0 }, { end_l + 1, 0 }, {})
+	local j = 1
+	for i = start_l, end_l do
+		-- second element of mark is the row
+		if marks[j][2] == i + 1 then -- i uses zero based indexing
+			j = j + 1
+		else
+			self.marks[#self.marks + 1] = api.nvim_buf_set_extmark(buf, ns, i, 0, {})
+		end
+	end
+end
+
+-- nodes should be arriving in order, therefore line_meta is correctly sorted
+function M:note_hl(buf, row, start_col, end_col)
+	local marks = api.nvim_buf_get_extmarks(buf, ns, { row, 0 }, { row, 0 }, {})
+	local line_meta = self.meta_by_mark[marks[1]]
+	line_meta[#line_meta+1] = { start_col = start_col, end_col = end_col }
+end
+
+function M:on_lines(buf, nodes, start_l, end_l)
+	self:ensure_placeholders(buf, start_l, end_l)
+
+	for _, node in pairs(nodes) do
+		local start_row, start_col, end_row, end_col = node:range()
+
+		if start_row == end_row then
+			self:note_hl(buf, start_row, start_col, end_col)
+		else
+			for row = start_row, end_row - 1 do
+				self:note_hl(buf, row, start_col, 0)
+				start_col = 0 -- only relevent for first line of comment
+			end
+			self:note_hl(buf, end_row, 0, end_col)
+		end
+	end
+
 	log.error("unimplemented")
 end
 
