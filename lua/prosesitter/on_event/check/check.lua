0de0a6464c57793a53f028559e299c5ee5aff2e9
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-07-29 15:43:43 +0200

    solves extmark starting at wrong position if spanning two areas

--- lua/prosesitter/on_event/check/check.lua
+++ lua/prosesitter/on_event/check/check.lua
@@ -66,6 +66,8 @@ function M.hl_iter(results, areas)
 	-- log.info(vim.inspect(areas))
 	local problems = vim.fn.json_decode(results)["stdin.md"]
 	log.info(vim.inspect(problems))
+	log.info(vim.inspect(areas))
+
 	if problems == nil then
 		-- TODO cleanup remove placeholders
 		return function()
@@ -91,19 +93,23 @@ function M.hl_iter(results, areas)
 
 		local lint_col, lint_end_col = unpack(problems[i]["Span"])
 		while lint_col > areas:next_col(j) do
+			log.info("lint_col: "..lint_col.." areas[j].col: "..areas[j].col)
 			j = j + 1
 		end
+		log.info("start: "..vim.inspect(areas[j]))
 		local hl_start = lint_col - areas[j].col + areas[j].row_col
 
-		while lint_end_col > areas:next_col(j) do
-			j = j + 1
+		local k = j
+		while lint_end_col > areas:next_col(k) do
+			k = k + 1
 		end
-		local hl_end = lint_end_col - areas[j].col + areas[j].row_col
+		local hl_end = lint_end_col - areas[k].col + areas[k].row_col
 
 		local severity = problems[i].Severity
 		local hl_group = cfg.vale_to_hl[severity]
 		local hover_txt = problems[i]["Message"]
 
+		log.info(vim.inspect(areas[j]))
 		return areas[j].buf_id, areas[j].row_id, hl_start, hl_end, hl_group, hover_txt
 	end
 end
