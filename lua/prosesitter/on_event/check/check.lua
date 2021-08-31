78baaba19ab519dff4d09b148a56ca29de301f11
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-08-31 22:19:12 +0200

    little bit more refactoring

--- lua/prosesitter/on_event/check/check.lua
+++ lua/prosesitter/on_event/check/check.lua
@@ -53,21 +53,31 @@ local function next_col(self, j)
 end
 
 local function start_col(problem, areas, j)
-		local lint_col, _ = unpack(problem["Span"])
-		while lint_col > areas:next_col(j) do
-			j = j + 1
-		end
-		local hl_start = lint_col - areas[j].col + areas[j].row_col
-		return hl_start, j
+	local lint_col, _ = unpack(problem["Span"])
+	while lint_col > areas:next_col(j) do
+		j = j + 1
+	end
+	local hl_start = lint_col - areas[j].col + areas[j].row_col
+	return hl_start, j
 end
 
 local function end_col(problem, areas, k)
-		local _, lint_end_col = unpack(problem["Span"])
-		while lint_end_col > areas:next_col(k) do
-			k = k + 1
+	local _, lint_end_col = unpack(problem["Span"])
+	while lint_end_col > areas:next_col(k) do
+		k = k + 1
+	end
+	local hl_end = lint_end_col - areas[k].col + areas[k].row_col
+	return hl_end
+end
+
+local function skip_to_next_problem(problems, i)
+	while problems[i].Span == next_problem_span(problems, i) do
+		if problems[i]["Message"] ~= problems[i + 1]["Message"] then
+			problems[i + 1]["Message"] = problems[i + 1]["Message"] .. "\n" .. problems[i]["Message"]
 		end
-		local hl_end = lint_end_col - areas[k].col + areas[k].row_col
-		return hl_end
+		i = i + 1
+	end
+	return i
 end
 
 local cfg = nil
@@ -88,14 +98,9 @@ function M.hl_iter(results, areas)
 			return nil
 		end
 
-		local hl = {}
-		while problems[i].Span == next_problem_span(problems, i) do
-			if problems[i]["Message"] ~= problems[i + 1]["Message"] then
-				problems[i + 1]["Message"] = problems[i + 1]["Message"] .. "\n" .. problems[i]["Message"]
-			end
-			i = i + 1
-		end
+		i = skip_to_next_problem(problems, i)
 
+		local hl = {}
 		hl.start_col, j = start_col(problems[i], areas, j)
 		hl.end_col = end_col(problems[i], areas, j)
 		hl.buf_id = areas[j].buf_id
