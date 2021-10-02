b65eb80a9d99b67ad0e6902bdf2b51a94319fe4f
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-10-02 15:03:45 +0200

    opens hover window after telescope selection

--- lua/prosesitter/telescope.lua
+++ lua/prosesitter/telescope.lua
@@ -4,16 +4,17 @@ local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 local action_state = require("telescope.actions.state")
-local action_set = require "telescope.actions.set"
+local action_set = require("telescope.actions.set")
+local log = require("prosesitter/log")
 local M = {}
 
 local function make_entries(shared)
 	local entries = {}
-	local buffer_marks = api.nvim_buf_get_extmarks(0, shared.ns_marks, 0, -1, {details = true})
-	local curr_buf = vim.fn.bufnr('%')
+	local buffer_marks = api.nvim_buf_get_extmarks(0, shared.ns_marks, 0, -1, { details = true })
+	local curr_buf = vim.fn.bufnr("%")
 	for _, mark in ipairs(buffer_marks) do
 		local id = mark[1]
-		entries[#entries+1] = {
+		entries[#entries + 1] = {
 			text = shared.mark_to_hover[id],
 			row = mark[2] + 1,
 			start_col = mark[3],
@@ -31,20 +32,20 @@ local function buf_path(bufnr)
 end
 
 local function fill_finder(shared)
-	return finders.new_table {
-      results = make_entries(shared),
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry.text,
-          ordinal = entry.text,
-		  path = buf_path(entry.buf),
-		  lnum = entry.row,
-		  start = entry.start_col,
-		  finish = entry.end_col,
-        }
-      end
-    }
+	return finders.new_table({
+		results = make_entries(shared),
+		entry_maker = function(entry)
+			return {
+				value = entry,
+				display = entry.text,
+				ordinal = entry.text,
+				path = buf_path(entry.buf),
+				lnum = entry.row,
+				start = entry.start_col,
+				finish = entry.end_col,
+			}
+		end,
+	})
 end
 
 function M.find(shared)
@@ -55,27 +56,18 @@ function M.find(shared)
 		sorter = conf.generic_sorter(opts),
 		previewer = conf.qflist_previewer(opts),
 		attach_mappings = function()
-		  action_set.select:enhance {
-			post = function()
-			  local selection = action_state.get_selected_entry()
-
-			  if selection.scode then
-				local scode = string.gsub(selection.scode, "[$]$", "")
-				scode = string.gsub(scode, [[\\]], [[\]])
-				scode = string.gsub(scode, [[\/]], [[/]])
-				scode = string.gsub(scode, "[*]", [[\*]])
-
-				vim.cmd "norm! gg"
-				vim.fn.search(scode)
-				vim.cmd "norm! zz"
-			  else
-				vim.api.nvim_win_set_cursor(0, { selection.lnum, 0 })
-			  end
-			end,
-		  }
-		  return true
+			action_set.select:enhance({
+				post = function()
+					local selection = action_state.get_selected_entry()
+					vim.api.nvim_win_set_cursor(0, { selection.lnum, selection.start + 1 })
+					local cb = function()
+						vim.lsp.util.open_floating_preview({ selection.display }, "markdown", {})
+					end
+					vim.schedule(cb)
+				end,
+			})
+			return true
 		end,
-
 	}):find()
 end
 
