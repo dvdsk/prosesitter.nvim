3a6d83958ecf6d0231aa37bdf9dd752dffdcf715
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-10-03 17:19:53 +0200

    adds some more default queries

--- lua/prosesitter/defaults.lua
+++ lua/prosesitter/defaults.lua
@@ -1,12 +1,16 @@
-local python_query = [[
-	[(string) (comment)+ ] @capture
-]]
-local rust_query = [[
-	[(line_comment)+ (block_comment) (string_literal)] @capture
-]]
-
 local M = {}
-M.query_by_ext = { rs = rust_query, py = python_query }
+
+M.query_by_ext = {
+	rs = "[(line_comment)+ (block_comment) (string_literal)] @capture",
+	py = "[(string) (comment)+ ] @capture",
+	lua = "[(comment)+ ] @capture",
+	c = "[(string_literal) (comment) ] @capture",
+	h = "[(string_literal) (comment) ] @capture",
+	cpp = "[(string_literal) (comment) ] @capture",
+	hpp = "[(string_literal) (comment) ] @capture",
+	tex = "[(text)+] @capture"
+}
+
 M.vale_cfg_ini = [==[
 # StylesPath = added by lua code during install
 MinAlertLevel = suggestion
