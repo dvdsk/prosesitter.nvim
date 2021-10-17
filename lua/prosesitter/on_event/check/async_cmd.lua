c638a76477cb3f47127220bbca90447c4df39642
dskleingeld <noreply@davidsk.dev>
2021-10-17 13:14:41 +0200

    fixes #32

--- lua/prosesitter/on_event/check/async_cmd.lua
+++ lua/prosesitter/on_event/check/async_cmd.lua
@@ -17,22 +17,21 @@ function M.dispatch_with_stdin(input, cmd, args, user_callback)
 	local stdout = uv.new_pipe(false)
 	local stderr = uv.new_pipe(false)
 
-	local output
+	local output = {}
 	local _, _ = uv.spawn(cmd, {
 		args = args,
 		stdio = { stdin, stdout, stderr },
 	}, vim.schedule_wrap(function(_, _) -- on exit
-		user_callback(output)
+		local string = table.concat(output, "")
+		user_callback(string)
 	end))
 
-	uv.read_start(stdout, vim.schedule_wrap(function(err, data)
+	uv.read_start(stdout, function(err, data)
 		assert(not err, err)
 		if data then
-			output = data
-		elseif err then
-			log.error("stdout err:", err)
+			output[# output+1] = data
 		end
-	end))
+	end)
 
 	uv.read_start(stderr, on_stderr)
 	uv.write(stdin, input)
