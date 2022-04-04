c61dc0359fc5a11ec6fa2baa6b6ebeb63cffa484
dvdsk <noreply@davidsk.dev>
2022-04-05 01:49:31 +0200

    fix links test case

--- lua/prosesitter/tests/preprocessing_spec.lua
+++ lua/prosesitter/tests/preprocessing_spec.lua
@@ -106,6 +106,6 @@ describe("preprocessing", function()
 			prepfn(buf, node, meta, lr)
         end
         local req = lr:build()
-		assert.are.same("", req.text)
+		assert.are.same("Queries are language specific strings that tell the treesitter parser how to find the prose in code. They look like this: For prosesitter to be able to automatically merge string only and comment lint targets we need the outer square brackets even if there is only a single pattern cought by a query. To see how queries match text see the Treesitter documentation. around the query You can easily create new ones using Treesitter Playground with its query editor. To add one or more queries to prosesitter make a table of extensions and the query that should be used and add that table to your config during setup under the key code. If a query already exists for a file extension your new query will replace it. language tool rules: https://community.languagetool.org/rule/list?lang=en ", req.text)
 	end)
 end)
