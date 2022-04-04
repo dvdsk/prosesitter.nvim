5bff1019fff3fdba7fdabd697b9219e2384db9c8
dvdsk <noreply@davidsk.dev>
2022-04-04 16:42:17 +0200

    refactor markdown_split_paragraph

--- lua/prosesitter/preprocessing/markdown.lua
+++ lua/prosesitter/preprocessing/markdown.lua
@@ -2,50 +2,65 @@ local util = require "prosesitter.preprocessing.util"
 
 M = {}
 
+local split = {}
+function split:new(buf, node, meta, req)
+	self.row_s, self.col_s, self.row_end, self.col_end = util.range(node, meta)
+	self.req = req
+	self.buf = buf
+end
+
+function split:add(row_start, col_start, row_end, col_end)
+	if row_start ~= row_end then
+		self.req:add_rows(self.buf, row_start, row_end, col_start, col_end)
+	elseif col_start ~= col_end then
+		self.req:add_row(self.buf, row_start, col_start, col_end)
+	end
+end
+
+function split:add_trimmed(trim, child)
+	local row_start, col_start, row_end, col_end = child:range()
+	self:add(self.row_s, self.col_s, row_start, col_start)
+	self:add(row_start, col_start+trim, row_end, col_end-trim)
+	self.row_s = row_end
+	self.col_s = col_end
+end
+
+function split:add_code(child)
+	local row_start, col_start, row_end, col_end = child:range()
+	self:add(self.row_s, self.col_s, row_start, col_start)
+	self.row_s = row_end
+	self.col_s = col_end
+	self.req:add_append_text(self.buf, self.row_s, "code")
+end
+
+function split:add_link(child)
+	local c_row_start, c_col_start, c_row_end, c_col_end = child:range()
+	self:add(self.row_s, self.col_s, c_row_start, c_col_start)
+	local text_child = child:named_child(0)
+	local row_start, col_start, row_end, col_end = text_child:range()
+	self:add(row_start, col_start, row_end, col_end)
+	self.row_s = c_row_end
+	self.col_s = c_col_end
+end
+
 -- assumes a single line
 local function markdown_split_paragraph(buf, node, meta, req)
-	local add = function(row_start, col_start, row_end, col_end)
-		if row_start ~= row_end then
-			req:add_rows(buf, row_start, row_end, col_start, col_end)
-		elseif col_start ~= col_end then
-			req:add_row(buf, row_start, col_start, col_end)
-		end
-	end
-
-	local curr_row_s, curr_col_s, par_row_end, par_col_end = util.range(node, meta)
-	local add_trimmed = function(trim, child)
-		local child_row_start, child_col_start, child_row_end, child_col_end = child:range()
-		add(curr_row_s, curr_col_s, child_row_start, child_col_start)
-		add(child_row_start, child_col_start+trim, child_row_end, child_col_end-trim)
-		curr_row_s = child_row_end
-		curr_col_s = child_col_end
-	end
-
+	split:new(buf, node, meta, req)
 	for child in node:iter_children() do
 		if child:type() == "emphasis" then
-			add_trimmed(1, child)
+			split:add_trimmed(1, child)
 		elseif child:type() == "strong_emphasis" then
-			add_trimmed(2, child)
+			split:add_trimmed(2, child)
 		elseif child:type() == "code_span" then
-			local child_row_start, child_col_start, child_row_end, child_col_end = child:range()
-			add(curr_row_s, curr_col_s, child_row_start, child_col_start)
-			curr_row_s = child_row_end
-			curr_col_s = child_col_end
-			req:add_append_text(buf, curr_row_s, "code")
+			split:add_code(child)
 		elseif child:type() == "inline_link" then
-			local child_row_start, child_col_start, child_row_end, child_col_end = child:range()
-			add(curr_row_s, curr_col_s, child_row_start, child_col_start)
-			local text_child = child:named_child(0)
-			local row_start, col_start, row_end, col_end = text_child:range()
-			add(row_start, col_start, row_end, col_end)
-			curr_row_s = child_row_end
-			curr_col_s = child_col_end
+			split:add_link(child)
 		end
 	end
 
-	if par_col_end > curr_col_s then
-		add(curr_row_s, curr_col_s, par_row_end, par_col_end)
-		req:add_append_text(buf, curr_row_s, " ")
+	if split.col_end > split.col_s then
+		split:add(split.row_s, split.col_s, split.row_end, split.col_end)
+		req:add_append_text(buf, split.row_s, " ")
 	end
 end
 
